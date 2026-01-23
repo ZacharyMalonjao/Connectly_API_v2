9fba797d34aa4353889b321675ed2a474fb2572b
ZacharyMalonjao <158264225+ZacharyMalonjao@users.noreply.github.com>
2026-01-23 14:36:37 +0800

    Week 3

--- connectly_project/posts/urls.py
+++ connectly_project/posts/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
-
+from .views import UserListCreate, PostListCreate,CommentListCreate
 urlpatterns=[
-    path('users/', views.get_users, name='get_users'),
-    path('users/create/', views.create_user, name='create_user'),
-    path('posts/', views.get_posts, name='get_posts'),
-    path('posts/create/',views.create_post, name='create_post'),
+#    path('users/', views.get_users, name='get_users'),
+#    path('users/create/', views.create_user, name='create_user'),
+#    path('posts/', views.get_posts, name='get_posts'),
+#    path('posts/create/',views.create_post, name='create_post'),
+
+
+    path('users/', UserListCreate.as_view(),name='user-list-create'),
+    path('', PostListCreate.as_view(),name ='post-list-create'),
+    path('comments/', CommentListCreate.as_view (), name='comment-list-create'),
 ]
+
+
+
+
+
+
--- connectly_project/posts/views.py
+++ connectly_project/posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import User
+from .models import User, Post, Comment
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
+
 # Create your views here.
 
 #Retrieve all users
@@ -48,3 +53,38 @@ def create_post(request):
             return JsonResponse({'error': 'Author not found'}, status=404)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+        
+class UserListCreate(APIView):
+    def get(self,request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+class PostListCreate(APIView):
+    def get(self, request):
+        posts= Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+class CommentListCreate(APIView):
+    def get(self, request):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
